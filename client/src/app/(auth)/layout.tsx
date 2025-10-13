ae49806acbe7a9f453787a064a45821204e03c81
Amir Hurtado <a.hurtado@utp.edu.co>
2025-10-13 15:31:09 -0500

    feat(layout): enhance layout with logo and user link, improve structure and responsiveness

--- client/src/app/(auth)/layout.tsx
+++ client/src/app/(auth)/layout.tsx
@@ -6,24 +6,40 @@ import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-dvh w-screen flex justify-center items-center p-2 md:p-8 relative">
-      <div className="absolute top-8 right-8 flex gap-2 items-center">
-        <ModeToggle />
+    <div className="h-dvh w-screen flex flex-col  p-2 md:p-8 relative">
+      <div className=" w-full flex   justify-between   items-start">
+        <div>
+          <Image src={"/logo.png"} alt="logo" width={90} height={90} />
+        </div>
 
-        <Link href={"https://github.com/amirhurtado"} className="border-1 border-gray-300 dark:border-gray-700 rounded-lg flex justify-center items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 transition-all ease-in ">
-          <Github strokeWidth={1} size={19.5} />
-        </Link>
+        <div className="flex gap-2">
+          <ModeToggle />
+
+          <Link
+            href={"https://github.com/amirhurtado"}
+            className="border-1 gap-2 border-gray-300 dark:border-gray-700 rounded-lg flex justify-center items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 transition-all ease-in "
+          >
+            <p className="text-sm">Amir Hurtado</p>
+            <Github strokeWidth={1} size={19.5} />
+          </Link>
+        </div>
       </div>
 
-      <div className="w-full md:w-1/2 flex justify-center">{children}</div>
+      <div className="flex justify-between ">
+        <div className="w-full md:w-1/2 flex items-center justify-center ">{children}</div>
 
-      <Image
-        src={"/sign-img.svg"}
-        alt="sign-img"
-        width={400}
-        height={300}
-        className="object-cover hidden md:block w-1/3 p-6"
-      />
+        <div className="w-1/2 hidden md:flex  justify-center ">
+          <Image
+          src={"/sign-img.svg"}
+          alt="sign-img"
+          width={400}
+          height={300}
+          className="object-cover w-1/2 "
+        />
+
+        </div>
+        
+      </div>
     </div>
   );
 };
--- client/src/app/(auth)/sign-in/page.tsx
+++ client/src/app/(auth)/sign-in/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const SignInPage = () => {
   return (
-    <div className='max-h-dvh w-full flex flex-col items-center overflow-auto '>
+    <div className='max-h-dvh w-full flex flex-col gap-2 items-center overflow-auto '> 
+
          <SignIn />
       
     </div>
