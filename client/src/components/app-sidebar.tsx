19aa3404e17d15ab26f68862b24d7f5faf8eb697
Amir Hurtado <a.hurtado@utp.edu.co>
2025-10-13 17:49:30 -0500

    feat(sidebar): integrate UserButton and enhance link transition effects in AppSidebar

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -10,6 +10,7 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar";
 import { navBar } from "@/constants";
+import { UserButton } from "@stackframe/stack";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -23,33 +24,50 @@ export function AppSidebar() {
         <Image src="/logo.png" width={50} height={50} alt="logo" />
       </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel className="uppercase mt-2">
-            <h2>Principal</h2>
-          </SidebarGroupLabel>
+        <div className="flex flex-col justify-between h-screen">
+          <SidebarGroup>
+            <SidebarGroupLabel className="uppercase mt-2">
+              <h2>Principal</h2>
+            </SidebarGroupLabel>
 
-          <SidebarGroupContent className="flex flex-col gap-2 pl-2 mt-2">
-            {navBar.map((item, i) => {
-              const Icon = item.icon;
-              const isActive = pathname === item.href;
+            <SidebarGroupContent className="flex flex-col gap-2 pl-2 mt-2">
+              {navBar.map((item, i) => {
+                const Icon = item.icon;
+                const isActive = pathname === item.href;
 
-              return (
-                <Link
-                  href={item.href}
-                  key={i}
-                  className={`flex gap-4 px-2 py-3 transition-colors rounded-lg ${
-                    isActive
-                      ? "bg-primary text-white  "
-                      : "hover:bg-accent text-gray-800 dark:text-gray-300"
-                  }`}
-                >
-                  <Icon strokeWidth={1} size={22} />
-                  <p className="text-md">{item.name}</p>
-                </Link>
-              );
-            })}
-          </SidebarGroupContent>
-        </SidebarGroup>
+                return (
+                  <Link
+                    href={item.href}
+                    key={i}
+                    className={`flex gap-4 px-2 py-3 transition-all rounded-lg hover:scale-[0.98] ease-in-out duration-200 ${
+                      isActive
+                        ? "bg-primary text-white  "
+                        : "hover:bg-accent text-gray-800 dark:text-gray-300"
+                    }`}
+                  >
+                    <Icon strokeWidth={1} size={22} />
+                    <p className="text-md">{item.name}</p>
+                  </Link>
+                );
+              })}
+            </SidebarGroupContent>
+          </SidebarGroup>
+
+          <div className="flex flex-col">
+            <SidebarGroupLabel className="uppercase mt-2">
+              <h2>Mi perfil</h2>
+            </SidebarGroupLabel>
+
+            <div className="pl-4 mt-2" >
+
+            <UserButton />
+
+
+            </div>
+
+          </div>
+        </div>
+
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
