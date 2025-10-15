1814b195b967237889fea58e225b9e9b649273e6
Amir Hurtado <a.hurtado@utp.edu.co>
2025-10-15 17:57:22 -0500

    feat(add-product): implement AddProductPage with Header component for product addition

--- client/src/app/(main)/add-product/page.tsx
+++ client/src/app/(main)/add-product/page.tsx
@@ -1,9 +1,10 @@
+import HeaderAddProduct from '@/features/add-products/components/Header'
 import React from 'react'
 
 const AddProductPage = () => {
   return (
-    <div>
-      AddProductPage    
+    <div className="flex flex-col w-full overflow-y-scroll md:max-h-dvh md:overflow-hidden gap-4 md:gap-8 p-2">
+      <HeaderAddProduct />
     </div>
   )
 }
---
+++ client/src/features/add-products/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderAddProduct = () => {
+  return (
+    <div>
+        <h1 className="text-xl md:text-2xl font-bold">Añadir nuevo producto</h1>
+        <p className="font-poppins mt-2 text-gray-500">
+          Completa el formulario para agregar un nuevo producto a tu inventario.
+        </p>
+      </div>
+  )
+}
+
+export default HeaderAddProduct
